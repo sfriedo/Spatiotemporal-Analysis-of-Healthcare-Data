40f256d39ebf12ce9b533dbbd9f58f83a6050a29
Siegfried Horschig <siegfried.horschig@student.hpi.de>
2017-12-07 12:23:04 +0100

    Add hana connection

---
+++ task1/python-server/hanaconnection.py
@@ -0,0 +1,40 @@
+import pyhdb
+import socket
+import json
+
+USER = ''
+PW = ''
+
+try:
+    with open('config.txt', 'rb') as cfg:
+        cfg = json.load(cfg)
+        USER = cfg['user']
+        PS = cfg['password']
+        print('Config loaded')
+except Exception:
+    print('Could not find config.txt, Database connection won\'t work!')
+
+
+class HanaConnection(object):
+    def __init__(self):
+        try:
+            self.connection = pyhdb.connect(
+                host='192.168.31.116',
+                port=30015,
+                user=USER,
+                password=PW,
+                autocommit=True
+            )
+            self.cursor = self.connection.cursor()
+        except socket.gaierror as e:
+            print('Database instance is not available! \n\n')
+            raise e
+
+    def __enter__(self):
+        return self.cursor
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if exc_type is not None:
+            print(exc_type, exc_value, traceback)
+        self.cursor.close()
+        self.connection.close()
